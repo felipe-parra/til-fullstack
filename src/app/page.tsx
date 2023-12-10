795948fc1883163cc21a6a832116abfb90d64696
Felipe Parra <fpr29.10@gmail.com>
2023-12-10 02:06:16 -0600

    Initialize stage 1 home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="card hero">
-        <p className="text-display-1 hero-title">
-          Let’s start authenticating <br /> with KindeAuth
+    <section className="w-full h-screen max-h-screen max-w-2xl p-4">
+      <h1 className="font-bold text-6xl">Sigue aprendiendo todos los dias</h1>
+      <p>
+        Lleva control de lo que estas aprendiendo, que este habito sea tu mejor aliado.
+      </p>
+      <article className="w-full max-w-xs h-12 my-2 self-end flex flex-col">
+        <Button asChild>
+          <Link href={"/hoy-aprendi"} className="w-full md:max-w-xs h-12 my-2 self-end">
+            💡  Hoy aprendi
+          </Link>
+        </Button>
+        <p className="text-xs font-thin">
+          🚀 Se la mejor persona para este 2024
         </p>
-        <p className="text-body-1 hero-tagline">Configure your app</p>
+      </article>
 
-        <Link
-          href="https://kinde.com/docs/sdks/nextjs-sdk"
-          target="_blank"
-          rel="noreferrer"
-          className="btn btn-light btn-big"
-        >
-          Go to docs
-        </Link>
-      </div>
-    </div>
+    </section>
   );
 }
