92c9559de8ee2a7e3746576c0f612bde599ee220
Felipe Parra <fpr29.10@gmail.com>
2023-12-10 02:25:00 -0600

    Updated styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="w-full h-screen max-h-screen max-w-2xl p-4">
-      <h1 className="font-bold text-6xl">Sigue aprendiendo todos los dias</h1>
+      <h1 className="font-bold text-6xl my-6">Sigue aprendiendo todos los dias</h1>
       <p>
         Lleva control de lo que estas aprendiendo, que este habito sea tu mejor aliado.
       </p>
-      <article className="w-full max-w-xs h-12 my-2 self-end flex flex-col">
+      <article className="w-full md:max-w-xs h-20 my-2 self-end flex flex-col">
         <Button asChild>
-          <Link href={"/hoy-aprendi"} className="w-full md:max-w-xs h-12 my-2 self-end">
+          <Link href={"/hoy-aprendi"} className="w-full md:max-w-xs h-16 my-2 self-end">
             💡  Hoy aprendi
           </Link>
         </Button>
