4653d0b72160d468967a00d61e6e81968811df59
Felipe Parra <fpr29.10@gmail.com>
2023-12-10 01:57:49 -0600

    Removed unnecessary comments

--- next.config.js
+++ next.config.js
@@ -5,15 +5,12 @@ module.exports = removeImports({});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // Google images https://lh3.googleusercontent.com/a/ACg8ocKYVKKpjRgmg9O1fdLe3qTEybXVFZgEdCUcVazqBkuZC2Y=s96-c
-
     remotePatterns: [
       {
         hostname: "lh3.googleusercontent.com",
       },
     ],
   },
-  // pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   experimental: { esmExternals: true },
 };
 
