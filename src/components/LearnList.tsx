9b52e93964dfe0f200f996978724d5576a1a5b62
Felipe Parra <fpr29.10@gmail.com>
2023-12-10 17:40:44 -0600

    Added logic to bring user learns

--- src/components/LearnList.tsx
+++ src/components/LearnList.tsx
@@ -1,38 +1,53 @@
 import { Learn } from '@/models/learn'
 import React, { Suspense } from 'react'
 import { ScrollArea, ScrollBar } from './ui/scroll-area'
-import { config } from '@/config'
 import { LearnItemComponent } from './LearnItem'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LearnModel } from '@/models'
+import dbConnect from '@/lib/mongo'
 
-const getAllLearns = async () => {
-  const res = await fetch(config.kindeSiteUrl + '/api/learn')
-
-  if (!res.ok) {
-    throw new Error("Error getting learns")
+const getAllLearns = async (userId?: string) => {
+  if (!userId) {
+    throw new Error("Check your credentials")
   }
-
-  const { data } = await res.json()
-  return data
+  await dbConnect()
+  const result = await LearnModel.find({ author: userId }).sort({ createdAt: "descending" })
+  const learns = JSON.parse(JSON.stringify(result))
+  console.log({ learns, userId })
+  return learns
 }
 
-
 export default async function LearnList() {
-  const learns = await getAllLearns()
+  const { getUser, isAuthenticated } = getKindeServerSession()
+  if (!isAuthenticated()) {
+    return <p>Something went wrong</p>
+  }
+
+  const user = await getUser()
+
+  const learns = await getAllLearns(user?.id)
+
   return (
     <>
-      <h2 className='text-4xl font-bold text-start max-w-md w-full my-4'>Mis aprendizajes</h2>
-      <ScrollArea className='w-full max-w-md h-screen max-h-80 whitespace-nowrap rounded-md border my-2'>
-        <section className='flex h-full max-h-80 space-x-4 p-4'>
-          <Suspense fallback={<p>Loading...</p>}>
-            {
-              learns && learns.map((learn: Learn, index: number) => (
-                <LearnItemComponent {...learn} key={'learn-id' + index} />
-              ))
-            }
-          </Suspense>
-        </section>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+      {
+        learns.length
+          ? <>
+            <h2 className='text-4xl font-bold text-start max-w-md w-full my-4'>Mis aprendizajes</h2>
+            <ScrollArea className='w-full max-w-md h-screen max-h-80 whitespace-nowrap rounded-md border my-2'>
+              <section className='flex h-full max-h-80 space-x-4 p-4'>
+                <Suspense fallback={<p>Loading...</p>}>
+                  {
+                    learns.map((learn: Learn, index: number) => (
+                      <LearnItemComponent {...learn} key={'learn-id' + index} />
+                    ))
+                  }
+                </Suspense>
+              </section>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </>
+          : null
+      }
     </>
   )
 }
