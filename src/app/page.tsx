a73f8b2a1c98a7bb3e7c832408fa7c84f6284a86
Felipe Parra <fpr29.10@gmail.com>
2023-12-10 17:37:48 -0600

    Updated with initial dinamically load constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+const LANDING_CONSTANTS = {
+  title: "Share what you learn every day",
+  subtitle: "A project where we believe in learning something new every day. Write and share your daily learnings with the world",
+  cta: {
+    title: "Today I Learned",
+    subtitle: "Join our community of lifelong learners.",
+    link: "/til"
+  }
+}
+
 export default function Home() {
   return (
-    <section className="w-full h-screen max-h-screen max-w-2xl p-4">
-      <h1 className="font-bold text-6xl my-6">Sigue aprendiendo todos los dias</h1>
-      <p>
-        Lleva control de lo que estas aprendiendo, que este habito sea tu mejor aliado.
+    <section className="w-full h-screen max-h-screen max-w-2xl mx-auto p-4 flex flex-col items-center justify-start">
+      <h1 className="font-bold text-6xl my-6 text-center">{LANDING_CONSTANTS.title}</h1>
+      <p className="text-center">
+        {LANDING_CONSTANTS.subtitle}
       </p>
-      <article className="w-full md:max-w-xs h-20 my-2 self-end flex flex-col">
+      <article className="w-full md:max-w-xs mx-auto h-20 my-2 self-end flex flex-col">
         <Button asChild>
-          <Link href={"/hoy-aprendi"} className="w-full md:max-w-xs h-16 my-2 self-end">
-            💡  Hoy aprendi
+          <Link href={LANDING_CONSTANTS.cta.link} className="w-full md:max-w-xs h-16 my-2 self-end text-lg">
+            💡 {LANDING_CONSTANTS.cta.title}
           </Link>
         </Button>
-        <p className="text-xs font-thin">
-          🚀 Se la mejor persona para este 2024
+        <p className="text-md font-thin">
+          🚀 {LANDING_CONSTANTS.cta.subtitle}
         </p>
       </article>
-
     </section>
   );
 }
